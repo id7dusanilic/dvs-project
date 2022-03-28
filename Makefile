472c04b82eecee6f54eae020ead83a30a91bf92a
Dusan Ilic <id213234m@student.etf.bg.ac.rs>
2022-03-28 15:35:05 +0200

    Updated software model test
    Test is now compatible with the software executed on the board.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CC = gcc
 CFLAGS = -g -Wall -Werror
+DEFINE = SOFTWARE_MODEL_ONLY
 
 LIB_DIR = lib
 INCLUDE_DIR = software_model
@@ -7,16 +8,18 @@ BUILD_DIR = build
 
 TARGET = main
 
+all: ${TARGET}
+
 $(BUILD_DIR)/%.o: $(INCLUDE_DIR)/%.c
 	mkdir -p $(BUILD_DIR)
-	$(CC) $(CFLAGS) -I$(INCLUDE_DIR) $< -c -o $@
+	$(CC) $(CFLAGS) -I. $< -c -o $@
 
 lib/lib%.so: $(BUILD_DIR)/%.o
 	mkdir -p $(LIB_DIR)
 	$(CC) $(CFLAGS) -shared -o $@ $^
 
-%: test/%.c $(BUILD_DIR)/utils.o
-	$(CC) $(CFLAGS) $^ -I$(INCLUDE_DIR) -o $(BUILD_DIR)/$@
+%: test/%.c $(BUILD_DIR)/utils.o $(BUILD_DIR)/bilinear_scaling.o
+	$(CC) $(CFLAGS) $^ -I. -D ${DEFINE} -o $(BUILD_DIR)/$@
 
 clean:
 	rm -rf $(BUILD_DIR) $(LIB_DIR)
--- realization/software/dvs22_g6_sw/main.c
+++ realization/software/dvs22_g6_sw/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef SOFTWARE_MODEL_ONLY
 #include "altera_avalon_performance_counter.h"
 #include "altera_avalon_sgdma_descriptor.h"
 #include "altera_avalon_sgdma.h"
@@ -10,19 +11,25 @@
 #include "system.h"
 
 #include "bilinear_scaling_hw.h"
+#endif
 #include "software_model/bilinear_scaling.h"
 #include "software_model/utils.h"
 
-#define ALTERA_HOSTFS_NAME_LEN (10) /* Length of ALTERA_HOSTFS_NAME"/". */
-#define MAX_STRLEN (255)            /* Maximum string length, used for static memory allocation. */
-#define SAME_AS_BEFORE '@'          /* Character used to signal that the same image is being used from previous input. */
-#define SAVE_FORMAT_BIN 'b'         /* Character indicating output image save format is bin */
-#define SAVE_FORMAT_PGM 'p'         /* Character indicating output image save format is pgm */
+#ifndef SOFTWARE_MODEL_ONLY
+#define PATH_PREPEND_LEN    (10)    /* Length of ALTERA_HOSTFS_NAME"/". */
+#define RESULT_HW_NAME      "result_hw"
+#else
+#define PATH_PREPEND_LEN    (0)
+#endif
+#define RESULT_SW_NAME      "result_sw"
+#define MAX_STRLEN          (255)   /* Maximum string length, used for static memory allocation. */
+#define SAME_AS_BEFORE      '@'     /* Character used to signal that the same image is being used from previous input. */
+#define SAVE_FORMAT_BIN     'b'     /* Character indicating output image save format is bin */
+#define SAVE_FORMAT_PGM     'p'     /* Character indicating output image save format is pgm */
 
 void get_input(
         char* input_filename,
         char* save_format,
-        char* output_filename,
         unsigned* ul_x,
         unsigned* ul_y,
         unsigned* dr_x,
@@ -30,6 +37,7 @@ void get_input(
         float* sx,
         float* sy);
 
+#ifndef SOFTWARE_MODEL_ONLY
 void transmit_callback_function(void* context) {
     uint16_t* tx_done = (uint16_t*) context;
     *tx_done = 0x0001;
@@ -39,15 +47,24 @@ void receive_callback_function(void* context) {
     uint16_t* rx_done = (uint16_t*) context;
     *rx_done = 0x0001;
 }
+#endif
 
 int main() {
 
+#ifndef SOFTWARE_MODEL_ONLY
     volatile uint16_t tx_done = 0x0000;
     volatile uint16_t rx_done = 0x0000;
+#endif
 
+#ifndef SOFTWARE_MODEL_ONLY
     /* Filename holders, prepended with hostfs name so that relative paths can be used. */
     char input_filename[MAX_STRLEN] = ALTERA_HOSTFS_NAME"/";
     char output_filename[MAX_STRLEN] = ALTERA_HOSTFS_NAME"/";
+#else
+    /* Filename holders. */
+    char input_filename[MAX_STRLEN];
+    char output_filename[MAX_STRLEN];
+#endif
 
     /* Output file format. */
     char save_format;
@@ -63,6 +80,7 @@ int main() {
 
     image_t image_in;
 
+#ifndef SOFTWARE_MODEL_ONLY
     /* SGDMA device instances */
     alt_sgdma_dev* sgdma_in = alt_avalon_sgdma_open(SGDMA_IN_NAME);
     alt_sgdma_dev* sgdma_out = alt_avalon_sgdma_open(SGDMA_OUT_NAME);
@@ -84,6 +102,7 @@ int main() {
          ALTERA_AVALON_SGDMA_CONTROL_IE_CHAIN_COMPLETED_MSK |
          ALTERA_AVALON_SGDMA_CONTROL_PARK_MSK),
         (void*)&rx_done);
+#endif
 
     /* Endless loop processing */
     for (;;) {
@@ -92,7 +111,6 @@ int main() {
         get_input(
             input_filename,
             &save_format,
-            output_filename,
             &ul_x,
             &ul_y,
             &dr_x,
@@ -101,7 +119,7 @@ int main() {
             &sy);
 
         /* If the input filename is SAME_AS_BEFORE don't load the image again. */
-        if ((input_filename[ALTERA_HOSTFS_NAME_LEN] != SAME_AS_BEFORE) || (num == 0)) {
+        if ((input_filename[PATH_PREPEND_LEN] != SAME_AS_BEFORE) || (num == 0)) {
             if (!image_in.data) image_free(image_in);
             printf("Loading image %s...\n", input_filename);
             image_in = bin2image(input_filename);
@@ -116,31 +134,53 @@ int main() {
         image_t input_segment = extract_segment(image_in, ul_x, ul_y, seg_height, seg_width);
         printf("Extracted segment.\n");
 
+#ifndef SOFTWARE_MODEL_ONLY
         /* Reset the performance counter unit. */
         PERF_RESET(PERFORMANCE_COUNTER_BASE);
         PERF_START_MEASURING(PERFORMANCE_COUNTER_BASE);
+#endif
 
+#ifndef SOFTWARE_MODEL_ONLY
         /* Software processing. */
         PERF_BEGIN(PERFORMANCE_COUNTER_BASE, 1);
+#endif
         image_t output_image_sw = bilinear_scaling_sw(input_segment, sx, sy);
+#ifndef SOFTWARE_MODEL_ONLY
         PERF_END(PERFORMANCE_COUNTER_BASE, 1);
+#endif
         printf("Image scaled (software).\n\n");
 
+#ifndef SOFTWARE_MODEL_ONLY
         /* Hardware processing. */
         PERF_BEGIN(PERFORMANCE_COUNTER_BASE, 2);
         image_t output_image_hw = bilinear_scaling_hw(input_segment, sx, sy, sgdma_in, sgdma_out, &tx_done, &rx_done);
         PERF_END(PERFORMANCE_COUNTER_BASE, 2);
         printf("Image scaled (hardware).\n\n");
+#endif
 
         if (save_format == SAVE_FORMAT_PGM) {
             /* Save to PGM for easier result display. */
+#ifndef SOFTWARE_MODEL_ONLY
+            sprintf(output_filename + PATH_PREPEND_LEN, "%s", RESULT_HW_NAME".pgm");
             save_to_pgm(output_filename, output_image_hw);
+            printf("\nImage %s saved.\n", output_filename);
+#endif
+            sprintf(output_filename + PATH_PREPEND_LEN, "%s", RESULT_SW_NAME".pgm");
+            save_to_pgm(output_filename, output_image_sw);
+            printf("\nImage %s saved.\n", output_filename);
         }
         else if (save_format == SAVE_FORMAT_BIN) {
+#ifndef SOFTWARE_MODEL_ONLY
+            sprintf(output_filename + PATH_PREPEND_LEN, "%s", RESULT_HW_NAME".bin");
             save_to_bin(output_filename, output_image_hw);
+            printf("\nImage %s saved.\n", output_filename);
+#endif
+            sprintf(output_filename + PATH_PREPEND_LEN, "%s", RESULT_SW_NAME".bin");
+            save_to_bin(output_filename, output_image_sw);
+            printf("\nImage %s saved.\n", output_filename);
         }
-        printf("\nImage %s saved.\n", output_filename);
 
+#ifndef SOFTWARE_MODEL_ONLY
         /* Print performance comparison results. */
         perf_print_formatted_report(
                 (void *)PERFORMANCE_COUNTER_BASE,
@@ -148,13 +188,16 @@ int main() {
                 2,
                 "Software",
                 "Hardware");
+#endif
 
         /* Update number of jobs done */
         num++;
 
         /* Deallocate used memory */
         image_free(output_image_sw);
+#ifndef SOFTWARE_MODEL_ONLY
         image_free(output_image_hw);
+#endif
         free(input_segment.data);     /* Input segment is a shallow copy. */
     }
 
@@ -166,7 +209,6 @@ int main() {
 void get_input(
         char* input_filename,
         char* save_format,
-        char* output_filename,
         unsigned* ul_x,
         unsigned* ul_y,
         unsigned* dr_x,
@@ -175,13 +217,9 @@ void get_input(
         float* sy) {
 
     printf("Enter input filename: ");
-    scanf("%s", input_filename + ALTERA_HOSTFS_NAME_LEN);
+    scanf("%s", input_filename + PATH_PREPEND_LEN);
     printf("\nEnter output image format: ");
     scanf(" %c", save_format);
-    if (*save_format == SAVE_FORMAT_BIN || *save_format == SAVE_FORMAT_PGM) {
-        printf("\nEnter output filename: ");
-        scanf("%s", output_filename + ALTERA_HOSTFS_NAME_LEN);
-    }
     printf("\nEnter endpoint coordinates: ");
     scanf("%u %u %u %u", ul_x, ul_y, dr_x, dr_y);
     assert((*dr_x > *ul_x) && (*dr_y > *ul_y));
