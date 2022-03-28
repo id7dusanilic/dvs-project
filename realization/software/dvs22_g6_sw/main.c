c48e561308f1f5d68ffe25368c6af4707cde23b8
Dusan Ilic <id213234m@student.etf.bg.ac.rs>
2022-03-28 12:39:21 +0200

    Hardware processing in main

--- realization/software/dvs22_g6_sw/main.c
+++ realization/software/dvs22_g6_sw/main.c
@@ -4,17 +4,24 @@
 #include <stdlib.h>
 
 #include "altera_avalon_performance_counter.h"
+#include "altera_avalon_sgdma_descriptor.h"
+#include "altera_avalon_sgdma.h"
+#include "altera_avalon_sgdma_regs.h"
 #include "system.h"
 
+#include "bilinear_scaling_hw.h"
+#include "software_model/bilinear_scaling.h"
 #include "software_model/utils.h"
 
 #define ALTERA_HOSTFS_NAME_LEN (10) /* Length of ALTERA_HOSTFS_NAME"/". */
 #define MAX_STRLEN (255)            /* Maximum string length, used for static memory allocation. */
 #define SAME_AS_BEFORE '@'          /* Character used to signal that the same image is being used from previous input. */
-#define SAVE_2_PGM (1)              /* If this is enabled (!= 0) the image will be saved in pgm instead of bin format. */
+#define SAVE_FORMAT_BIN 'b'         /* Character indicating output image save format is bin */
+#define SAVE_FORMAT_PGM 'p'         /* Character indicating output image save format is pgm */
 
 void get_input(
         char* input_filename,
+        char* save_format,
         char* output_filename,
         unsigned* ul_x,
         unsigned* ul_y,
@@ -23,12 +30,27 @@ void get_input(
         float* sx,
         float* sy);
 
+void transmit_callback_function(void* context) {
+    uint16_t* tx_done = (uint16_t*) context;
+    *tx_done = 0x0001;
+}
+
+void receive_callback_function(void* context) {
+    uint16_t* rx_done = (uint16_t*) context;
+    *rx_done = 0x0001;
+}
 
 int main() {
+
+    volatile uint16_t tx_done = 0x0000;
+    volatile uint16_t rx_done = 0x0000;
 
     /* Filename holders, prepended with hostfs name so that relative paths can be used. */
     char input_filename[MAX_STRLEN] = ALTERA_HOSTFS_NAME"/";
     char output_filename[MAX_STRLEN] = ALTERA_HOSTFS_NAME"/";
+
+    /* Output file format. */
+    char save_format;
 
     /* Endpoint coordinates for the input segment. */
     unsigned ul_x, ul_y, dr_x, dr_y;
@@ -41,12 +63,35 @@ int main() {
 
     image_t image_in;
 
+    /* SGDMA device instances */
+    alt_sgdma_dev* sgdma_in = alt_avalon_sgdma_open(SGDMA_IN_NAME);
+    alt_sgdma_dev* sgdma_out = alt_avalon_sgdma_open(SGDMA_OUT_NAME);
+
+    /* Registering sgdma_out transmit callback function. */
+    alt_avalon_sgdma_register_callback(
+        sgdma_out,
+        &transmit_callback_function,
+        (ALTERA_AVALON_SGDMA_CONTROL_IE_GLOBAL_MSK |
+         ALTERA_AVALON_SGDMA_CONTROL_IE_CHAIN_COMPLETED_MSK |
+         ALTERA_AVALON_SGDMA_CONTROL_PARK_MSK),
+        (void*)&tx_done);
+
+    /* Registering sgdma_in receive callback function. */
+    alt_avalon_sgdma_register_callback(
+        sgdma_in,
+        &receive_callback_function,
+        (ALTERA_AVALON_SGDMA_CONTROL_IE_GLOBAL_MSK |
+         ALTERA_AVALON_SGDMA_CONTROL_IE_CHAIN_COMPLETED_MSK |
+         ALTERA_AVALON_SGDMA_CONTROL_PARK_MSK),
+        (void*)&rx_done);
+
     /* Endless loop processing */
     for (;;) {
 
         /* Populate required fields from stdin. */
         get_input(
             input_filename,
+            &save_format,
             output_filename,
             &ul_x,
             &ul_y,
@@ -63,6 +108,7 @@ int main() {
             printf("Image %s loaded.\n", input_filename);
         }
 
+        /* Segment dimensions. */
         unsigned seg_width = dr_x - ul_x + 1;
         unsigned seg_height = dr_y - ul_y + 1;
 
@@ -76,32 +122,39 @@ int main() {
 
         /* Software processing. */
         PERF_BEGIN(PERFORMANCE_COUNTER_BASE, 1);
-        image_t output_image = bilinear_scaling_sw(input_segment, sx, sy);
+        image_t output_image_sw = bilinear_scaling_sw(input_segment, sx, sy);
         PERF_END(PERFORMANCE_COUNTER_BASE, 1);
-        printf("Image scaled.\n\n");
+        printf("Image scaled (software).\n\n");
 
+        /* Hardware processing. */
+        PERF_BEGIN(PERFORMANCE_COUNTER_BASE, 2);
+        image_t output_image_hw = bilinear_scaling_hw(input_segment, sx, sy, sgdma_in, sgdma_out, &tx_done, &rx_done);
+        PERF_END(PERFORMANCE_COUNTER_BASE, 2);
+        printf("Image scaled (hardware).\n\n");
 
-#if SAVE_2_PGM != 0
-        /* Save to PGM for easier result display. */
-        save_to_pgm(output_filename, output_image);
-#else
-        save_to_bin(output_filename, output_image);
-#endif
+        if (save_format == SAVE_FORMAT_PGM) {
+            /* Save to PGM for easier result display. */
+            save_to_pgm(output_filename, output_image_hw);
+        }
+        else if (save_format == SAVE_FORMAT_BIN) {
+            save_to_bin(output_filename, output_image_hw);
+        }
         printf("\nImage %s saved.\n", output_filename);
-
 
         /* Print performance comparison results. */
         perf_print_formatted_report(
                 (void *)PERFORMANCE_COUNTER_BASE,
                 alt_get_cpu_freq(),
-                1,
-                "processing_sw");
+                2,
+                "Software",
+                "Hardware");
 
         /* Update number of jobs done */
         num++;
 
         /* Deallocate used memory */
-        image_free(output_image);
+        image_free(output_image_sw);
+        image_free(output_image_hw);
         free(input_segment.data);     /* Input segment is a shallow copy. */
     }
 
@@ -112,6 +165,7 @@ int main() {
 
 void get_input(
         char* input_filename,
+        char* save_format,
         char* output_filename,
         unsigned* ul_x,
         unsigned* ul_y,
@@ -122,8 +176,12 @@ void get_input(
 
     printf("Enter input filename: ");
     scanf("%s", input_filename + ALTERA_HOSTFS_NAME_LEN);
-    printf("\nEnter output filename: ");
-    scanf("%s", output_filename + ALTERA_HOSTFS_NAME_LEN);
+    printf("\nEnter output image format: ");
+    scanf(" %c", save_format);
+    if (*save_format == SAVE_FORMAT_BIN || *save_format == SAVE_FORMAT_PGM) {
+        printf("\nEnter output filename: ");
+        scanf("%s", output_filename + ALTERA_HOSTFS_NAME_LEN);
+    }
     printf("\nEnter endpoint coordinates: ");
     scanf("%u %u %u %u", ul_x, ul_y, dr_x, dr_y);
     assert((*dr_x > *ul_x) && (*dr_y > *ul_y));
